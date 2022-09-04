b230f6f314ba0162f80ba075cd9daad4ae7f53bd
William Kang <contact@williamk.ga>
2022-09-04 02:54:55 -0400

    deps & error embed

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,5 @@
+import { EmbedBuilder } from 'discord.js';
+
 export default async (client, interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
@@ -9,6 +11,18 @@ export default async (client, interaction) => {
     await command.execute(client, interaction);
   } catch (error) {
     client.logger.error(error.stack ?? error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+
+    return interaction.reply({ ephemeral: true, embeds: [
+      new EmbedBuilder()
+        .setColor('#FF0000')
+        .setTimestamp()
+        .setTitle('Please report this on GitHub')
+        .setURL('https://github.com/william5553/quinton/issues')
+        .addFields(
+          { name: '**Command**', value: interaction.commandName, inline: true },
+          { name: '**Options**', value: `${JSON.stringify(interaction.options.data, { depth: 2 })}` }
+        )
+        .setDescription(`**Stack Trace:**\n\`\`\`${error.stack ?? error}\`\`\``)
+    ]});
   }
 };
